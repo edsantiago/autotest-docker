99764612b487e956b1d4346eb1063fbcb9d9dd7b
Ed Santiago <santiago@redhat.com>
2016-12-16 12:21:30 -0700

    fixup! Write rpm -q files for all dockers
    ... docker, docker-latest, and (sigh) an "active" file
    containing the rpm of the currently-enabled one.

--- pretests/log_versions/log_versions.py
+++ pretests/log_versions/log_versions.py
@@ -35,7 +35,9 @@ class log_versions(subtest.Subtest):
                 % (docker_version.client, docker_version.server))
         self.loginfo("Found %s", info)
         self.write_sysinfo('docker_version', info + "\n")
-        self.write_sysinfo('docker_rpm', self._docker_rpm())
+        self.write_sysinfo('docker_rpm_active', self._rpmq(which_docker()))
+        self.write_sysinfo('docker_rpm_current', self._rpmq('docker'))
+        self.write_sysinfo('docker_rpm_latest', self._rpmq('docker-latest'))
 
     def write_sysinfo(self, filename, content):
         """
@@ -46,5 +48,5 @@ class log_versions(subtest.Subtest):
             outfile.write(content)
 
     @staticmethod
-    def _docker_rpm():
-        return utils.run("rpm -q %s" % which_docker()).stdout
+    def _rpmq(package_name):
+        return utils.run("rpm -q %s" % package_name).stdout
